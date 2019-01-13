3728a3b85757c9a17748b964b7182d4c631a07ee
paulo henrique santana costa <falarcompaulohenrique@hotmail.com>
2019-01-13 12:42:13 -0300

    Primeiro commit

---
+++ TesteConsoleAPP/TesteConsoleAPP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteConsoleAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo do uso do GitHub");
+            Console.ReadKey();
+        }
+    }
+}
